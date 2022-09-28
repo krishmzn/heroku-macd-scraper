838bd64ca23cb833f443c72f7acda7f7177c0fdd
krishmzn11 <kimmhrz@gmail.com>
2022-09-28 09:42:03 +0545

    new commit

--- scraper.py
+++ scraper.py
@@ -17,11 +17,11 @@ import schedule
 import time
 
 chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 
 def job():
     # email list of people we want ot email to
